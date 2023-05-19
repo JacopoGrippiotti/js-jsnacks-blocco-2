d1e4358796447ea857bdb74c9bfe8171eca847d3
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-19 17:57:12 +0200

    structuring script.js file to complete jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -4,5 +4,33 @@ let min = 1
 
 let numGenerator = Math.floor(Math.random()*(max - min + 1)) + min
 
+let guessed = false
+
+let higherNumberToGuess = false
+
+let lowerNumberToGuess = false
+
+
+while(guessed == false){
+
+    let userNumber = prompt('inserisci il tuo numero')
+
+    if ( userNumber < numGenerator  ){
+
+        higherNumberToGuess = true
+    }
+    else if (userNumber > numGenerator){
+
+        lowerNumberToGuess = true
+    }
+    else if (userNumber == numGenerator){
+        guessed = true
+    }
+
+    
+}
+
+
+
 console.log(numGenerator)
 
