4c5ef6016a581ed3209571ac0c499401e3101411
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-19 16:54:11 +0200

    structured script.js file to complete jsnack1

---
+++ jsnack1/script.js
@@ -0,0 +1,28 @@
+let emptyArray = []
+
+let sumMore = false
+
+let sumOfNumbers = 0
+
+let i = 0
+
+while (sumMore == false){
+
+    
+    let userNumber = parseInt(prompt("inserisci un numero"))
+
+    emptyArray.push(userNumber)
+
+    sumOfNumbers += emptyArray[i]
+
+   
+    if(sumOfNumbers >= 50){
+
+        sumMore = true
+    } 
+
+    i++
+
+}
+
+console.log(emptyArray)
