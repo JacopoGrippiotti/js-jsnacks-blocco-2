4957f2f575f0696a0aa7f2cd4ef5fcfaa88c2299
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-22 12:42:22 +0200

    definitely fixed jsnack4

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -7,10 +7,10 @@ function lengthAnalizer(lunghezzaPrimaParola, lunghezzaSecondaParola){
     if(lunghezzaPrimaParola == lunghezzaSecondaParola){
         console.log('le parole sono di dimensioni identiche', primaParola, secondaParola)
 
-    else{
+    }else {
         console.log('le parole sono di dimensioni differenti mi dispiace')
     }
-    }
+
 }
 
 lengthAnalizer(primaParola.length, secondaParola.length);
