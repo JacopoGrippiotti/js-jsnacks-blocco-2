d6544df8cd7ba36d15ad2dce2ba6a105f47f9a34
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-23 12:54:27 +0200

    completed bonus for jsnack-plus

--- jsnack-plus/script.js
+++ jsnack-plus/script.js
@@ -1,16 +1,27 @@
 const list = document.getElementById("unorderedList")
 
-function createListElement(tagname, content){
+function createListElement(tagname, content,){
 
     let listElement = document.createElement(tagname)
 
     listElement.append(content)
-
+    
     return listElement
     
 }
 
 for(let i = 0; i < 10; i++){
 
-    list.appendChild(createListElement("li", "ciao"))
+   const liElement = createListElement("li", "ciao")
+
+   liElement.addEventListener('click', function(){
+
+    liElement.classList.toggle('underlined')
+
+   });
+   
+   list.appendChild(liElement)
+   
+
+ 
 }
